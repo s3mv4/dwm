887ba0b626d76178b81b195bd5ea988bef43a1ee
s3m <s3m@archlinux.org>
2022-09-21 09:12:24 +0200

    changed font to nerd font

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const unsigned int gappx     = 7;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "Hack Nerd Font:style=Regular:size=10", "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";

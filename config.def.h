3d5f51935f83e87cd2ed04f2eea7e1a02f1e3dab
s3m <s3m@archlinux.org>
2022-09-26 10:20:57 +0200

    changed dmenufont to nerdfont

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font:style=Regular:size=10", "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "Hack Nerd Font:style=Regular:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";

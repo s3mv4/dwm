c4cac8729a5cb8a332d6cb4fe2f58b81fa0d5b19
s3m <s3m@archlinux.org>
2022-12-21 19:14:07 +0100

    added icons to some tags

--- config.h
+++ config.h
@@ -60,7 +60,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "3", "4", "5", "6", "7", "", "" };
 
 static const Rule rules[] = {
     /* xprop(1):

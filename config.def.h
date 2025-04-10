ed23fc123cf17ff6f83d76ffc3fc7b058a5e76c7
s3m <s3m@archlinux.org>
2025-04-10 19:36:07 +0200

    added multimedia keybindings

--- config.def.h
+++ config.def.h
@@ -60,6 +60,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+#include <X11/XF86keysym.h>
+
 static const Key keys[] = {
 	/* modifier                     key              function        argument */
 	{ MODKEY,                       XK_p,            spawn,          {.v = dmenucmd } },
@@ -94,7 +96,12 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_bracketleft,                  6)
 	TAGKEYS(                        XK_exclam,                       7)
 	TAGKEYS(                        XK_equal,                        8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,                 {0} },
+	{ MODKEY|ShiftMask,             XK_q,            quit,           {0} },
+	{ 0, XF86XK_AudioLowerVolume,                    spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%; kill -35 $(pidof dwmblocks)") },
+	{ 0, XF86XK_AudioRaiseVolume,                    spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%; kill -35 $(pidof dwmblocks)") },
+	{ 0, XF86XK_AudioMute,                           spawn,          SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle; kill -35 $(pidof dwmblocks)") },
+	{ 0, XF86XK_MonBrightnessDown,                   spawn,          SHCMD("brightnessctl set 5%-; kill -36 $(pidof dwmblocks)") },
+	{ 0, XF86XK_MonBrightnessUp,                     spawn,          SHCMD("brightnessctl set 5%+; kill -36 $(pidof dwmblocks)") },
 };
 
 /* button definitions */

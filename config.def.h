659a1d95bd65251ad29a901e61a85472bd86d570
s3m <s3m@archlinux.org>
2025-06-15 09:32:11 +0200

    patched barpadding

--- config.def.h
+++ config.def.h
@@ -8,6 +8,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 7;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 5;        /* vertical padding for statusbar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "JetBrainsMonoNL Nerd Font:size=10" };
 static const char dmenufont[]       = "JetBrainsMonoNL Nerd Font:size=10";
 static const char col_gray1[]       = "#2e3440";

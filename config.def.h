b5c723e7ec14f037a2fa71aa3c363b61697fabea
s3m <s3m@archlinux.org>
2025-06-15 09:26:30 +0200

    patched statuspadding

--- config.def.h
+++ config.def.h
@@ -6,6 +6,8 @@ static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 7;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 5;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "JetBrainsMonoNL Nerd Font:size=10" };
 static const char dmenufont[]       = "JetBrainsMonoNL Nerd Font:size=10";
 static const char col_gray1[]       = "#2e3440";

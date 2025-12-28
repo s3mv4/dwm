9592fb31013eac27eebc7aa5856ebcb8838aeee4
s3m <s3m@archlinux.org>
2025-12-28 12:18:14 +0100

    added window rules for firefox and spotify

--- config.def.h
+++ config.def.h
@@ -32,8 +32,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	// { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	// { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Spotify",  NULL,       NULL,       1 << 2,       0,           -1 },
 	{ NULL,       NULL,       NULL,       0,            0,           -1 },
 };
 
